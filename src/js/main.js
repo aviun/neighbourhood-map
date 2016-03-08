48337cfd98b651180a2bf55d71040dcc7d68ed4c
aviun <anastasiia.viun@gmail.com>
2016-03-07 20:49:59 -0500

    Bouncing feature added, manipulated both from list view and from map view

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,116 @@
 //Google API script is calling this function to launch the app
-initMap = function(){
+
+var map = '';
+
+var initMap = function () {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: new google.maps.LatLng(50.423123, 30.526792),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
     viewModel.init();
+
+};
+
+var viewModel = {
+    self: this,
+    locations: ko.observableArray(),
+
+    toggleMarker: function (location) {
+        viewModel.disableMarkers();
+        location.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+    },
+
+    disableMarkers: function() {
+        for (var i = 0; i < this.locations().length; i++) {
+            this.locations()[i].marker.setAnimation(null);
+        }
+    },
+
+    fillLocations: function () {
+        for (var i = 0; i < model.locations.length; i++) {
+            this.locations.push(model.locations[i]);
+        }
+    },
+
+    init: function () {
+        model.init();
+        this.fillLocations();
+        ko.applyBindings(viewModel);
+    }
+};
+
+var model = {
+    self: this,
+    locations: [
+        {
+            title: 'Home',
+            lat: 50.38,
+            lng: 30.46
+        },
+        {
+            title: 'Maidan Nezalezhnosti',
+            lat: 50.44,
+            lng: 30.52
+        },
+        {
+            title: 'Mariinsky Palace',
+            lat: 50.44,
+            lng: 30.53
+        },
+        {
+            title: 'Kyivo Pechersk Lavra',
+            lat: 50.43,
+            lng: 30.55
+        },
+        {
+            title: 'Richard Lionheart Castle',
+            lat: 50.46,
+            lng: 30.51
+        }
+    ],
+
+    setContent: function () {
+        for (var i = 0; i < this.locations.length; i++) {
+            this.locations[i].infowindow = new google.maps.InfoWindow({
+                content: location.title + " " + location.content
+            });
+        }
+    },
+
+    addMarkers: function () {
+        for (var i = 0; i < this.locations.length; i++) {
+            this.locations[i].marker = this.createMarker(this.locations[i], i);
+        }
+    },
+
+    toggleBounce: function () {
+        if (this.getAnimation() !== null) {
+            this.setAnimation(null);
+        } else {
+            this.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    },
+
+    createMarker: function (location) {
+
+        var marker = new google.maps.Marker({
+            title: location.title,
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(location.lat, location.lng)
+        });
+        marker.addListener('click', this.toggleBounce);
+        return marker;
+    },
+
+
+    init: function () {
+        this.setContent();
+        this.addMarkers();
+    }
+
+
 };
